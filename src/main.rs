cea9fa6856993f9dabddf0dd1085c993cca8769b
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-05-18 22:02:12 +0100

    refactor: implement dynamic port allocation for server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
 use zero_to_prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
+    run(listener)?.await
 }
