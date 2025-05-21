afb3782a6040426bec68c092ff09c8debdc7a229
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-05-21 18:42:24 +0100

    feat(db): implement PostgreSQL connection

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
-use zero_to_prod::startup::run;
+use zero_to_prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
-    run(listener)?.await
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).expect("Failed to bind to random port");
+
+    run(listener, connection_pool)?.await
 }
