bb1458e7ab110529fee61f0ec8f849a650ef20ca
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-05-23 21:44:58 +0100

    feat(telemetry): integrate telemetry module with application startup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
-use zero_to_prod::{configuration::get_configuration, startup::run};
+use zero_to_prod::{
+    configuration::get_configuration,
+    startup::run,
+    telemetry::{get_subscriber, init_subscriber},
+};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // Subscriber receives all span and event data and decides how to process it for output
+    let subscriber = get_subscriber("zero_to_prod".into(), "info".into());
+    init_subscriber(subscriber);
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
