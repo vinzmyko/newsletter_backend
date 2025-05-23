99a3954b76b6f7e797a622f45b30ba6b20153222
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-05-23 21:34:34 +0100

    fix(scripts): correct echo message for database init

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -45,4 +45,4 @@ export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${
 sqlx database create
 sqlx migrate run
 
->&2 echo "Postgres has been migrated, read to go!"
+>&2 echo "Postgres has been migrated, ready to go!"
