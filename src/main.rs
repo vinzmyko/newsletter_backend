ff1e54762b105f0002c36d7865ffe381378210ff
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-05-31 17:22:59 +0100

    feat: implement early timeout when connecting to Postgres

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero_to_prod::{
     configuration::get_configuration,
@@ -14,9 +14,10 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool =
-        PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to create Postgres connection pool.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool.");
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
