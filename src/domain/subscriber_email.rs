08e66cfb3585a8f0558b28e363f6959d530fcf30
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-05 20:23:44 +0100

    test(domain): add unit tests for email validation

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -18,3 +18,50 @@ impl AsRef<str> for SubscriberEmail {
         &self.0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::SubscriberEmail;
+    use claim::assert_err;
+    use fake::{Fake, faker::internet::en::SafeEmail};
+    use rand::{SeedableRng, rngs::StdRng};
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(pub String);
+
+    // How quickcheck should generate the test data
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let seed = u64::arbitrary(g);
+            //Create a seeded RNG that fake can use
+            let mut rng = StdRng::seed_from_u64(seed);
+            let email = SafeEmail().fake_with_rng(&mut rng);
+
+            Self(email)
+        }
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let email = String::from("");
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_at_symbol_is_rejected() {
+        let email = "ursuladomain.com".to_string();
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[test]
+    fn email_missing_subject_is_rejected() {
+        let email = String::from("@domail.com");
+        assert_err!(SubscriberEmail::parse(email));
+    }
+
+    // Calls function default 100 times and if error is met narrows it down
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
+        SubscriberEmail::parse(valid_email.0).is_ok()
+    }
+}
