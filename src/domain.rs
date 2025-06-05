ae07d850d28ac844e0a165a1faa3ec0e57041e0a
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-05 15:49:07 +0100

    refactor: refactor parse function to return a Result<SubscriberName, string>

--- src/domain.rs
+++ src/domain.rs
@@ -5,19 +5,20 @@ pub struct NewSubscriber {
     pub name: SubscriberName,
 }
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
-    pub fn parse(s: String) -> SubscriberName {
+    pub fn parse(s: String) -> Result<SubscriberName, String> {
         let is_empty_or_whitespace = s.trim().is_empty();
         let is_too_long = s.graphemes(true).count() > 256;
         let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            panic!("{} is not a valid subscriber name.", s);
+            Err(format!("'{}' is not a valid subscriber name.", s))
         } else {
-            Self(s)
+            Ok(Self(s))
         }
     }
 }
