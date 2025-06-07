6674af23444cc30afcb9276f0aa41f05d9702ee9
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-07 19:22:12 +0100

    refactor: update to incorporate email client functionality

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero_to_prod::{
     configuration::get_configuration,
+    email_client::EmailClient,
     startup::run,
     telemetry::{get_subscriber, init_subscriber},
 };
@@ -24,6 +25,23 @@ async fn main() -> std::io::Result<()> {
             .acquire_timeout(std::time::Duration::from_secs(2))
             .connect_lazy_with(configuration.database.with_db())
     };
+
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let timeout = configuration.email_client.timeout();
+    let email_client = EmailClient::new(
+        configuration
+            .email_client
+            .base_url
+            .parse()
+            .expect("Invalid base URL in configuration."),
+        sender_email,
+        configuration.email_client.authorisation_token,
+        timeout,
+    );
+
     sqlx::migrate!("./migrations")
         .run(&connection_pool)
         .await
@@ -35,5 +53,7 @@ async fn main() -> std::io::Result<()> {
     let address = format!("{}:{}", configuration.application.host, port);
     let listener = TcpListener::bind(address).expect("Failed to bind to random port");
 
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await?;
+
+    Ok(())
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,18 +1,27 @@
-use actix_web::{App, HttpServer, dev::Server, web};
+use actix_web::{App, HttpServer, dev::Server, web, web::Data};
 use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-use crate::routes::{health_check, subscribe};
+use crate::{
+    email_client::EmailClient,
+    routes::{health_check, subscribe},
+};
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    let db_pool = web::Data::new(db_pool);
+pub fn run(
+    listener: TcpListener,
+    db_pool: PgPool,
+    email_client: EmailClient,
+) -> Result<Server, std::io::Error> {
+    let db_pool = Data::new(db_pool);
+    let email_client = Data::new(email_client);
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
+            .app_data(email_client.clone())
     })
     .listen(listener)?
     .run();
