d0e958ad4e1db9660d0ff939a71701e97e2a5bf9
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-08 22:29:58 +0100

    feat(start-up): create application struct to wrap server and port

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,80 @@
-use actix_web::{App, HttpServer, dev::Server, web, web::Data};
-use sqlx::PgPool;
-use std::net::TcpListener;
-use tracing_actix_web::TracingLogger;
-
 use crate::{
+    configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
     routes::{health_check, subscribe},
 };
+use actix_web::{App, HttpServer, dev::Server, web, web::Data};
+use sqlx::{PgPool, postgres::PgPoolOptions};
+use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
+
+pub struct Application {
+    port: u16,
+    server: Server,
+}
+
+impl Application {
+    pub async fn build(configuration: Settings) -> Result<Self, std::io::Error> {
+        let connection_pool = get_connection_pool(&configuration.database).await;
+
+        let sender_email = configuration
+            .email_client
+            .sender()
+            .expect("Invalid sender email address.");
+        let timeout = configuration.email_client.timeout();
+        let email_client = EmailClient::new(
+            configuration
+                .email_client
+                .base_url
+                .parse()
+                .expect("Invalid base URL in configuration."),
+            sender_email,
+            configuration.email_client.authorisation_token,
+            timeout,
+        );
+
+        let requested_port = if configuration.application.port == 0 {
+            0
+        } else {
+            std::env::var("PORT")
+                .unwrap_or_else(|_| configuration.application.port.to_string())
+                .parse::<u16>()
+                .expect("Failed to parse PORT")
+        };
+
+        let address = format!("{}:{}", configuration.application.host, requested_port);
+        let listener = TcpListener::bind(&address)?;
+        let designated_port = listener.local_addr().unwrap().port();
+        let server = run(listener, connection_pool, email_client)?;
+
+        Ok(Self {
+            port: designated_port,
+            server,
+        })
+    }
+
+    pub fn port(&self) -> u16 {
+        self.port
+    }
+
+    pub async fn run_until_stopped(self) -> Result<(), std::io::Error> {
+        self.server.await
+    }
+}
+
+pub async fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
+    if let Ok(database_url) = std::env::var("DATABASE_URL") {
+        PgPoolOptions::new()
+            .acquire_timeout(std::time::Duration::from_secs(2))
+            .connect(&database_url)
+            .await
+            .expect("Failed to connect to Postgres")
+    } else {
+        PgPoolOptions::new()
+            .acquire_timeout(std::time::Duration::from_secs(2))
+            .connect_lazy_with(configuration.with_db())
+    }
+}
 
 pub fn run(
     listener: TcpListener,
