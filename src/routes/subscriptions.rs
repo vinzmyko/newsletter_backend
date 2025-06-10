52f9fe7c27a25a29b4fb2a31ca4a4989b14cacf6
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-10 21:07:48 +0100

    refactor(subscriptions): create send confirmation email function
    To clean up the request handler and segment the telemetry so it's more obvious which part is happening.

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -46,20 +46,36 @@ pub async fn subscribe(
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     };
-
-    if email_client
-        .send_email(
-            new_subscriber.email,
-            "Welcome!",
-            "Welcome to our newsletter",
-            "Welcome to our newsletter",
-        )
+    if send_confirmation_email(&email_client, new_subscriber)
         .await
         .is_err()
     {
         return HttpResponse::InternalServerError().finish();
     }
     HttpResponse::Ok().finish()
+}
+
+#[tracing::instrument(
+    name = "Send a confirmation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    let confirmation_link = "https://my-api.com/subscriptions/confirm";
+    let html_body = &format!(
+        "Welcome to our newsletter!<br />\
+            Click <a href=\"{}\">here</a> to confirm your subscription.",
+        confirmation_link
+    );
+    let plain_body = &format!(
+        "Welcome to our newsletter!\nVisit {} to confirm your subscription.",
+        confirmation_link
+    );
+    email_client
+        .send_email(new_subscriber.email, "Welcome!", &html_body, &plain_body)
+        .await
 }
 
 #[tracing::instrument(
