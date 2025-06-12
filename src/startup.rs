d3be75ce4e8bc78ac905f78c2f5c98b926f738e2
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-12 18:36:45 +0100

    feat(subscriptions-confirm) add subscriptions confirm endpoint event handlers

--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,7 @@
 use crate::{
     configuration::{DatabaseSettings, Settings},
     email_client::EmailClient,
-    routes::{health_check, subscribe},
+    routes::{confirm, health_check, subscribe},
 };
 use actix_web::{App, HttpServer, dev::Server, web, web::Data};
 use sqlx::{PgPool, postgres::PgPoolOptions};
@@ -99,6 +99,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
