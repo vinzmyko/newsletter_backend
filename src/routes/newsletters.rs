9f7958f5b6332b7ec4fbf88cdbc6db85eff1c80d
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-18 20:01:07 +0100

    feat(newsletters): implement naive approach for newsletters endpoints
    Missing out on:\n-Security\n-Performance\n-Fault Tolerance\n-Retry Safety\n-No chance to edit or review email you are sending out

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,4 +1,18 @@
-use actix_web::{HttpResponse, web};
+use actix_web::{HttpResponse, ResponseError, http::StatusCode, web};
+use anyhow::Context;
+use sqlx::PgPool;
+
+use crate::{domain::SubscriberEmail, email_client::EmailClient};
+
+struct ConfirmedSubscriber {
+    email: SubscriberEmail,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum PublishError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
 
 #[derive(serde::Deserialize)]
 pub struct BodyData {
@@ -12,6 +26,72 @@ pub struct Content {
     text: String,
 }
 
-pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+impl ResponseError for PublishError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+}
+
+pub async fn publish_newsletter(
+    body: web::Json<BodyData>,
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> Result<HttpResponse, PublishError> {
+    let subscribers = get_confirmed_subscribers(&pool).await?;
+    for subscriber in subscribers {
+        match subscriber {
+            Ok(subscriber) => {
+                email_client
+                    .send_email(
+                        &subscriber.email,
+                        &body.title,
+                        &body.content.html,
+                        &body.content.text,
+                    )
+                    .await
+                    // .with_context() is lazy, only takes a closure and only called in case of error
+                    // .context() would allocate the string everytime we send an email out, in this case
+                    // only when delivery fails
+                    .with_context(|| {
+                        format!("Failed to send newsletter issue to {}", subscriber.email)
+                    })?;
+            }
+            Err(error) => {
+                tracing::warn!(
+                    error.cause_chain = ?error,
+                    "Skipping a confirmed subscirber. \
+                        Their stored contact details are invalid",
+                );
+            }
+        }
+    }
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
+async fn get_confirmed_subscribers(
+    pool: &PgPool,
+) -> Result<Vec<Result<ConfirmedSubscriber, anyhow::Error>>, anyhow::Error> {
+    // Maps the retrieved rows type of the first argument
+    let rows = sqlx::query!(
+        r#"
+        SELECT email
+        FROM subscriptions
+        WHERE status = 'confirmed'
+        "#,
+    )
+    .fetch_all(pool)
+    .await?;
+
+    let confirmed_subscribers = rows
+        .into_iter()
+        .map(|r| match SubscriberEmail::parse(r.email) {
+            Ok(email) => Ok(ConfirmedSubscriber { email }),
+            Err(error) => Err(anyhow::anyhow!(error)),
+        })
+        .collect();
+
+    Ok(confirmed_subscribers)
 }
