7453f774921fd77eafdd86449dd628d4308e1097
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-18 22:41:37 +0100

    test(newsletters): add requests missing authorisation are rejected test

--- tests/api/newsletters.rs
+++ tests/api/newsletters.rs
@@ -113,6 +113,30 @@ async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     app.get_confirmation_links(email_request)
 }
 
+#[tokio::test]
+async fn requests_missing_authorisation_are_rejected() {
+    let app = spawn_app().await;
+
+    let response = reqwest::Client::new()
+        .post(format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
 async fn create_confirmed_subscriber(app: &TestApp) {
     let confirmation_link = create_unconfirmed_subscriber(app).await;
     // Mimicing user clicking on the link
