c864b90e869823ded2de76911e97662589c71f21
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-21 21:59:33 +0100

    test(helpers): update TestUser::store() to match our algorithm params

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,6 @@
 use argon2::{
     password_hash::SaltString,
-    {Argon2, PasswordHasher},
+    {Algorithm, Argon2, Params, PasswordHasher, Version},
 };
 use once_cell::sync::Lazy;
 use secrecy::ExposeSecret;
@@ -58,10 +58,15 @@ impl TestUser {
 
     async fn store(&self, pool: &PgPool) {
         let salt = SaltString::generate(&mut rand::thread_rng());
-        let password_hash = Argon2::default()
-            .hash_password(self.password.as_bytes(), &salt)
-            .unwrap()
-            .to_string();
+        let password_hash = Argon2::new(
+            Algorithm::Argon2id,
+            Version::V0x13,
+            Params::new(15000, 2, 1, None).unwrap(),
+        )
+        .hash_password(self.password.as_bytes(), &salt)
+        .unwrap()
+        .to_string();
+
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash)
             VALUES ($1, $2, $3)",
