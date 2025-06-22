560f37069b3bed49bbe02637dcab50cf60a94261
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-22 22:18:46 +0100

    feat(login): implement error handling for the POST /login endpoint

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,8 +1,12 @@
-use actix_web::{HttpResponse, http::header::LOCATION, web};
+use actix_web::{
+    HttpResponse, ResponseError,
+    http::{StatusCode, header::LOCATION},
+    web,
+};
 use secrecy::Secret;
 use sqlx::PgPool;
 
-use crate::authentication::{Credentials, validate_credentials};
+use crate::authentication::{AuthError, Credentials, validate_credentials};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,25 +14,44 @@ pub struct FormData {
     password: Secret<String>,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum LoginError {
+    #[error("Authentication failed")]
+    AuthError(#[source] anyhow::Error),
+    #[error("Something went wrong")]
+    UnexpectedError(#[from] anyhow::Error),
+}
+
+impl ResponseError for LoginError {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            LoginError::AuthError(_) => StatusCode::UNAUTHORIZED,
+        }
+    }
+}
+
 #[tracing::instrument(
     skip(form, pool),
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
-pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+pub async fn login(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, LoginError> {
     let credentials = Credentials {
         username: form.0.username,
         password: form.0.password,
     };
     tracing::Span::current().record("username", tracing::field::display(&credentials.username));
-    match validate_credentials(credentials, &pool).await {
-        Ok(user_id) => {
-            tracing::Span::current().record("user_id", tracing::field::display(&user_id));
-            return HttpResponse::SeeOther()
-                .insert_header((LOCATION, "/"))
-                .finish();
-        }
-        Err(_) => {
-            todo!()
-        }
-    }
+    let user_id = validate_credentials(credentials, &pool)
+        .await
+        .map_err(|e| match e {
+            AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
+            AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
+        })?;
+    tracing::Span::current().record("user_id", tracing::field::display(&user_id));
+    Ok(HttpResponse::SeeOther()
+        .insert_header((LOCATION, "/"))
+        .finish())
 }
