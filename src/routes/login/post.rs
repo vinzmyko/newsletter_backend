c4ec7540920eb38cb2ba2654544819600991d853
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-22 21:29:39 +0100

    feat(login): implement user validation when accessing the POST /login endpoint

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,34 @@
-use actix_web::HttpResponse;
+use actix_web::{HttpResponse, http::header::LOCATION, web};
+use secrecy::Secret;
+use sqlx::PgPool;
 
-pub async fn login() -> HttpResponse {
-    HttpResponse::Ok().finish()
+use crate::authentication::{Credentials, validate_credentials};
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    username: String,
+    password: Secret<String>,
+}
+
+#[tracing::instrument(
+    skip(form, pool),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
+pub async fn login(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let credentials = Credentials {
+        username: form.0.username,
+        password: form.0.password,
+    };
+    tracing::Span::current().record("username", tracing::field::display(&credentials.username));
+    match validate_credentials(credentials, &pool).await {
+        Ok(user_id) => {
+            tracing::Span::current().record("user_id", tracing::field::display(&user_id));
+            return HttpResponse::SeeOther()
+                .insert_header((LOCATION, "/"))
+                .finish();
+        }
+        Err(_) => {
+            todo!()
+        }
+    }
 }
