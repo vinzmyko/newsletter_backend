72115b14bb18079d93ae9d6bd6f4c77229403616
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-26 17:37:54 +0100

    feat(session-state): add typed wrapper for Session
    Adds better type safety and more compile time guarantee

---
+++ src/session_state.rs
@@ -0,0 +1,34 @@
+use std::future::{Ready, ready};
+
+use actix_session::{Session, SessionExt, SessionGetError, SessionInsertError};
+use actix_web::{FromRequest, HttpRequest, dev::Payload};
+use uuid::Uuid;
+
+pub struct TypedSession(Session);
+
+impl TypedSession {
+    const USER_ID_KEY: &'static str = "user_id";
+
+    pub fn renew(&self) {
+        self.0.renew();
+    }
+
+    pub fn insert_user_id(&self, user_id: Uuid) -> Result<(), SessionInsertError> {
+        self.0.insert(Self::USER_ID_KEY, user_id)
+    }
+
+    pub fn get_user_id(&self) -> Result<Option<Uuid>, SessionGetError> {
+        self.0.get(Self::USER_ID_KEY)
+    }
+}
+
+// When we try to access a variable of type TypedSession from the event handler's parameters it
+// will call the from_request function here. Essentially wrapping it in our type.
+impl FromRequest for TypedSession {
+    type Error = <Session as FromRequest>::Error;
+    type Future = Ready<Result<TypedSession, Self::Error>>;
+
+    fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
+        ready(Ok(TypedSession(req.get_session())))
+    }
+}
