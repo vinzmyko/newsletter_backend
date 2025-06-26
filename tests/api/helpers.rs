1846ed08b7c73f997a61924ba48a7878447518de
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-26 16:33:18 +0100

    test(helpers): add TestApp::get_admin_dashboard() helper method

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -83,6 +83,17 @@ impl TestUser {
 }
 
 impl TestApp {
+    pub async fn get_admin_dashboard(&self) -> String {
+        self.api_client
+            .get(format!("{}/admin/dashboard", &self.address))
+            .send()
+            .await
+            .expect("Failed to exectute request.")
+            .text()
+            .await
+            .unwrap()
+    }
+
     pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
     where
         Body: serde::Serialize,
