41a67b1a97ffc2f5849d5fcdedb7ba83c09173e0
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-26 18:07:23 +0100

    test(helpers): split get_admin_dashboard function into Response and String outputs

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -83,15 +83,16 @@ impl TestUser {
 }
 
 impl TestApp {
-    pub async fn get_admin_dashboard(&self) -> String {
+    pub async fn get_admin_dashboard(&self) -> reqwest::Response {
         self.api_client
             .get(format!("{}/admin/dashboard", &self.address))
             .send()
             .await
             .expect("Failed to exectute request.")
-            .text()
-            .await
-            .unwrap()
+    }
+
+    pub async fn get_admin_dashboard_html(&self) -> String {
+        self.get_admin_dashboard().await.text().await.unwrap()
     }
 
     pub async fn post_login<Body>(&self, body: &Body) -> reqwest::Response
