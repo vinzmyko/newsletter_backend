1e3e3fccc351ab8bc379ca7e20e547493859fa41
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-26 18:31:38 +0100

    chore(db): add initial seed user migration

---
+++ migrations/20250626170938_seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (user_id, username, password_hash)
+VALUES (
+    '3ac73582-d39e-4efb-8f89-fe29606b3d6a',
+    'admin',
+    '$argon2id$v=19$m=15000,t=2,p=1$TKQlbV8gAgj4oYqnFnlAuA$a6lx1GGC9XlIulu0fUYDDBaD5Q0rwK2qNVMtfywqNYo'
+);
