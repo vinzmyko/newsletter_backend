41b8846f5831f98ce5b2c42d706cbd4d688e033c
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-28 16:58:52 +0100

    test(helpers): add TestApp::post_logout impl method

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -83,6 +83,14 @@ impl TestUser {
 }
 
 impl TestApp {
+    pub async fn post_logout(&self) -> reqwest::Response {
+        self.api_client
+            .post(format!("{}/admin/logout", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
     pub async fn get_change_password_html(&self) -> String {
         self.get_change_password().await.text().await.unwrap()
     }
