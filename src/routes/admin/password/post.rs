3e7101ae863783c1ea2b80733d4441fcc68254e5
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-28 17:18:27 +0100

    refactor(change-password): implement reject_anonymous_users() function for repeated calls

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,8 @@
-use actix_web::{HttpResponse, web};
+use actix_web::{HttpResponse, error::InternalError, web};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
+use uuid::Uuid;
 
 use crate::{
     authentication::{AuthError, Credentials, validate_credentials},
@@ -41,11 +42,7 @@ pub async fn change_password(
     session: TypedSession,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = session.get_user_id().map_err(e500)?;
-    if user_id.is_none() {
-        return Ok(see_other("/login"));
-    };
-    let user_id = user_id.unwrap();
+    let user_id = reject_anonymous_users(session).await?;
     let new_password = match ValidNewPassword::parse(form.new_password.expose_secret()) {
         Ok(password) => password,
         Err(error) => {
@@ -93,6 +90,17 @@ pub async fn change_password(
         .map_err(e500)?;
     FlashMessage::info("Your password has been changed.").send();
     Ok(see_other("/admin/password"))
+}
+
+async fn reject_anonymous_users(session: TypedSession) -> Result<Uuid, actix_web::Error> {
+    match session.get_user_id().map_err(e500)? {
+        Some(user_id) => Ok(user_id),
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged in");
+            Err(InternalError::from_response(e, response).into())
+        }
+    }
 }
 
 #[cfg(test)]
