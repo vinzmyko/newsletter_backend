69111b0077e3c22b200012adf9b33cdba9c71382
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-29 21:00:11 +0100

    test(helpers): update post_newsletter() for session authentication
    Previously used basic auth

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -161,14 +161,17 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
-    pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+    pub async fn post_newsletter<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
         self.api_client
-            .post(format!("{}/newsletters", &self.address))
-            .basic_auth(&self.test_user.username, Some(&self.test_user.password))
-            .json(&body)
+            .post(format!("{}/admin/newsletter", &self.address))
+            .form(body)
             .send()
             .await
-            .expect("Failed to execute request.")
+            .expect("Failed to exectute request.")
+    }
 
     pub async fn get_newsletter(&self) -> reqwest::Response {
         self.api_client
