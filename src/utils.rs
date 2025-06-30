96767a0decdf69931357f2a1f4026840c7e1b681
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-06-30 22:08:31 +0100

    feat(utils): create return server error 400 function

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,9 @@
 use actix_web::{HttpResponse, http::header::LOCATION};
 
+pub fn e400<T: std::fmt::Debug + std::fmt::Display + 'static>(e: T) -> actix_web::Error {
+    actix_web::error::ErrorBadRequest(e)
+}
+
 pub fn e500<T>(e: T) -> actix_web::Error
 where
     T: std::fmt::Debug + std::fmt::Display + 'static,
