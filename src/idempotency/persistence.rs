8045a6e6474faac85302398221f09a0c4a3d4b77
Vinz Myko Del Rosario <vinzmykodelrosario@gmail.com>
2025-07-02 22:12:43 +0100

    feat(idempotency): add try_processing for concurrent request handling

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -1,5 +1,5 @@
 use actix_web::{HttpResponse, body::to_bytes, http::StatusCode};
-use sqlx::PgPool;
+use sqlx::{PgPool, Postgres, Transaction, postgres::PgHasArrayType};
 use uuid::Uuid;
 
 use super::IdempotencyKey;
@@ -11,6 +11,49 @@ struct HeaderPairRecord {
     value: Vec<u8>,
 }
 
+pub enum NextAction {
+    StartProcessing(Transaction<'static, Postgres>),
+    ReturnSavedResponse(HttpResponse),
+}
+
+impl PgHasArrayType for HeaderPairRecord {
+    fn array_type_info() -> sqlx::postgres::PgTypeInfo {
+        sqlx::postgres::PgTypeInfo::with_name("_header_pair")
+    }
+}
+
+pub async fn try_processing(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+) -> Result<NextAction, anyhow::Error> {
+    let mut transaction = pool.begin().await?;
+    let n_inserted_rows = sqlx::query!(
+        r#"
+    INSERT INTO idempotency (
+        user_id,
+        idempotency_key,
+        created_at
+    )
+    VALUES ($1, $2, now())
+    ON CONFLICT DO NOTHING
+    "#,
+        user_id,
+        idempotency_key.as_ref()
+    )
+    .execute(&mut transaction)
+    .await?
+    .rows_affected();
+    if n_inserted_rows > 0 {
+        Ok(NextAction::StartProcessing(transaction))
+    } else {
+        let saved_response = get_saved_response(pool, idempotency_key, user_id)
+            .await?
+            .ok_or_else(|| anyhow::anyhow!("We expected a saved response, we didn't find it"))?;
+        Ok(NextAction::ReturnSavedResponse(saved_response))
+    }
+}
+
 pub async fn get_saved_response(
     pool: &PgPool,
     idempotency_key: &IdempotencyKey,
@@ -19,9 +62,9 @@ pub async fn get_saved_response(
     let saved_response = sqlx::query!(
         r#"
     SELECT
-        response_status_code,
-        response_headers as "response_headers: Vec<HeaderPairRecord>",
-        response_body
+        response_status_code as "response_status_code!",
+        response_headers as "response_headers!: Vec<HeaderPairRecord>",
+        response_body as "response_body!"
     FROM idempotency
     WHERE
         user_id = $1 AND
@@ -43,4 +86,46 @@ pub async fn get_saved_response(
     } else {
         Ok(None)
     }
+}
+
+pub async fn save_response(
+    mut transaction: Transaction<'static, Postgres>,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    http_response: HttpResponse,
+) -> Result<HttpResponse, anyhow::Error> {
+    let (response_head, body) = http_response.into_parts();
+    let body = to_bytes(body).await.map_err(|e| anyhow::anyhow!("{}", e))?;
+    let status_code = response_head.status().as_u16() as i16;
+    let headers = {
+        let mut h = Vec::with_capacity(response_head.headers().len());
+        for (name, value) in response_head.headers().iter() {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            h.push(HeaderPairRecord { name, value });
+        }
+        h
+    };
+    sqlx::query_unchecked!(
+        r#"
+        UPDATE idempotency
+        SET
+            response_status_code = $3,
+            response_headers = $4,
+            response_body = $5
+        WHERE
+            user_id = $1 AND
+            idempotency_key = $2
+        "#,
+        user_id,
+        idempotency_key.as_ref(),
+        status_code,
+        headers,
+        body.as_ref(),
+    )
+    .execute(&mut transaction)
+    .await?;
+
+    let http_response = response_head.set_body(body).map_into_boxed_body();
+    Ok(http_response)
 }
